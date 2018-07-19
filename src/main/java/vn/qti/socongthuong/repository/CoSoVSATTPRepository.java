09b34b8b0bf88e2d889384427d2105e6ef0d4b2a
Bui Hoa <hoabuiyt@gmail.com>
2018-07-19 15:07:12 +0700

    Revert "thêm DanhMucNNKD"
    This reverts commit b22ebba87acc25e0d11a40f0c241ab0614c57c5d.

--- src/main/java/vn/qti/socongthuong/repository/CoSoVSATTPRepository.java
+++
@@ -1,8 +0,0 @@
-package vn.qti.socongthuong.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import vn.qti.socongthuong.model.CoSoVSATTP;
-
-public interface CoSoVSATTPRepository extends CrudRepository<CoSoVSATTP,String>{
-	
-}
--- src/main/java/vn/qti/socongthuong/repository/DanhMucNNKDRepository.java
+++
@@ -1,8 +0,0 @@
-package vn.qti.socongthuong.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import vn.qti.socongthuong.model.DanhMucNNKD;
-
-public interface DanhMucNNKDRepository extends CrudRepository<DanhMucNNKD,String>{
-	
-}
