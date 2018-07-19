b22ebba87acc25e0d11a40f0c241ab0614c57c5d
Bui Hoa <hoabuiyt@gmail.com>
2018-07-19 15:01:47 +0700

    thêm DanhMucNNKD
    thêm Add + List All DanhmucNNKD

---
+++ src/main/java/vn/qti/socongthuong/repository/CoSoVSATTPRepository.java
@@ -0,0 +1,8 @@
+package vn.qti.socongthuong.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import vn.qti.socongthuong.model.CoSoVSATTP;
+
+public interface CoSoVSATTPRepository extends CrudRepository<CoSoVSATTP,String>{
+	
+}
