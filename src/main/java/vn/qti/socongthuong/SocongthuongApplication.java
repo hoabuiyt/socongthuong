76bf3d10ca194ca9027028027bfa9d12f34f23ba
Bui Hoa <hoabuiyt@gmail.com>
2018-07-19 13:59:16 +0700

    init project
    new project Sping Boot JPA SoCongThuong

---
+++ src/main/java/vn/qti/socongthuong/SocongthuongApplication.java
@@ -0,0 +1,12 @@
+package vn.qti.socongthuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocongthuongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocongthuongApplication.class, args);
+	}
+}
